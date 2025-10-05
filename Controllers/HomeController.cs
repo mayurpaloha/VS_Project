efecb582aee3280d7011c9dc1bce63b463ee0555
Mayur <mayurpaloha@gmail.com>
2025-10-05 19:30:07 +0530

    Update footer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,19 +19,50 @@ namespace Agro_Saffron.Controllers
 
         public async Task<IActionResult> Index()
         {
-            //var featuredProducts = await _context.Products
-            //    .Where(p => p.IsActive)
-            //    .Include(p => p.Category)
-            //    .Take(8)
-            //    .ToListAsync();
+            var featuredProducts = await _context.Products
+                .Where(p => p.IsActive)
+                .Include(p => p.Category)
+                .Take(8)
+                .ToListAsync();
 
-            //return View(featuredProducts);
-            return View();
+            return View(featuredProducts);
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        // Add these new actions
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Terms()
+        {
+            return View();
+        }
+
+        public IActionResult Shipping()
+        {
+            return View();
+        }
+
+        public IActionResult Returns()
+        {
+            return View();
+        }
+
+        public IActionResult Support()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = System.Diagnostics.Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
